b45b396bc8cfdc4c3a0a988a6f5f270f9686de12
wedfrgt <191498695@qq.com>
2021-03-06 22:41:21 +0800

    Site updated: 2021-03-06 22:41:20

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -42,7 +42,7 @@ function loadWidget(config) {
 	let userAction = false,
 		userActionTimer,
 		messageTimer,
-		messageArray = ["好久不见，日子过得好快呢……", "大坏蛋！你都多久没理人家了呀，嘤嘤嘤～", "嗨～快来逗我玩吧！", "拿小拳拳锤你胸口！", "记得把小家加入 Adblock 白名单哦！"];
+		messageArray = ["喂，有人吗？", "大坏蛋！你都多久没理人家了呀，嘤嘤嘤～", "嗨～快来逗我玩吧！", "拿小拳拳锤你胸口！", "再不理我，我就要顺着网线去找你了！"];
 	window.addEventListener("mousemove", () => userAction = true);
 	window.addEventListener("keydown", () => userAction = true);
 	setInterval(() => {
